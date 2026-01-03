ad3efba8b3e60c6ff0918d61ca8f1320efb68f8a
wappi vignoly <votre-email@gmail.com>
2026-01-04 00:05:49 +0100

    Fix CSS path and styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    base: '/Flow_femme/', // Explicit base path for GitHub Pages
     server: {
         port: 5175,
         host: true
